45eb837c0f2e942f98ddba0b172c664f94057c4f
RiNn2T <anhhangtruchung@gmail.com>
2025-04-17 19:06:04 +0700

    Add files via upload

--- b8btvnb5.js
+++ b8btvnb5.js
@@ -1,21 +1,23 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
+// Next/previous controls
 function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
+// Thumbnail image controls
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
     let i;
-    const slides = document.getElementsByClassName("slide");
-    const dots = document.getElementsByClassName("dot");
+    let slides = document.getElementsByClassName("slide");
+    let dots = document.getElementsByClassName("dot");
     
-    if (n > slides.length) { slideIndex = 1; }
-    if (n < 1) { slideIndex = slides.length; }
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
     
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
@@ -25,11 +27,11 @@ function showSlides(n) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
     
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
 }
 
-// Auto slide change every 5 seconds
+// Tự động chuyển slide mỗi 5 giây
 setInterval(() => {
     plusSlides(1);
 }, 5000);
